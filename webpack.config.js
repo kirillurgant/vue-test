5f34be4edd1daa7f3b672eab87e03b1d348d6b32
Kirill Urgant <etozhkirill@yandex.ru>
2018-03-09 17:54:41 +0300

    init first webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const PATHS = {
+    src: path.join(__dirname, 'src'),
+    dist: path.join(__dirname, 'bundles/js')
+};
+
+module.exports = {
+    entry: {
+        'bundle': path.join(PATHS.src, 'main.js')
+    },
+    output: {
+        filename: '[name].js',
+        path: path.join(PATHS.dist)
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {}
+                    // other vue-loader options go here
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    devServer: {
+        port: 3000
+    },
+    mode: 'development'
+};
